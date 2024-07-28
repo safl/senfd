68751148980c4267f19cb21ce7c828a18a68d616
Simon A. F. Lund <os@safl.dk>
2024-07-28 14:52:05 +0200

    feat(documents): add helper returning all document-classes
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

---
+++ src/senfd/documents/__init__.py
@@ -0,0 +1,6 @@
+from senfd.documents.categorized import CategorizedFigureDocument
+from senfd.documents.figure import FigureDocument
+
+
+def get_document_classes():
+    return [FigureDocument, CategorizedFigureDocument]
