da807b11672270a95f427ff9f40a55866f741e15
Simon A. F. Lund <os@safl.dk>
2024-07-28 21:06:22 +0200

    feat: represent errors using namedtuple
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

---
+++ src/senfd/errors.py
@@ -0,0 +1,5 @@
+from collections import namedtuple
+
+TableCaptionError = namedtuple('TableCaptionError', ['table_nr', 'caption', 'message'])
+
+TableOfFiguresError = namedtuple("TableOfFiguresError", ['caption', 'message'])
