5949a95147d3682d49bf896089d4751e63372ba2
Simon A. F. Lund <os@safl.dk>
2024-07-29 07:08:43 +0200

    fix(tests): test pipeline instead of individual converters
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python3
 from pathlib import Path
 
-from senfd.documents.categorized import CategorizedFigureDocument
+import senfd.pipeline
 
 
 def test_module(tmp_path):
 
     path_example = Path("example")
-    paths = list(path_example.glob("*.json"))
-    assert len(paths) > 0, f"No json available for testing in path({path_example})"
+    paths = list(path_example.glob("*"))
+    assert len(paths) > 0, f"No documents available for testing in path({path_example})"
 
     for path in paths:
-        document = CategorizedFigureDocument.from_figure_document_file(path)
-        assert document.is_valid()
-
-        json_str = document.to_json()
-        assert json_str, "Failed producing a string of JSON"
-
-        document.to_json_file(tmp_path / "test.json")
-
-        from_disk = CategorizedFigureDocument.parse_file(tmp_path / "test.json")
-        assert document == from_disk
+        errors = senfd.pipeline.process(path, tmp_path)
+        assert not errors
