8f5c2044b5277920f3e38ffad58d0a33e7108c73
Simon A. F. Lund <os@safl.dk>
2024-07-29 07:11:06 +0200

    fix(pipeline): add immediate processing of converted doc.
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- src/senfd/pipeline.py
+++ src/senfd/pipeline.py
@@ -9,17 +9,14 @@ CONVERTERS = [FromDocx, FromFigureDocument]
 def process(input: Path, output: Path):
     all_errors = []
 
-    next_stage = []
     for converter in CONVERTERS:
         if not converter.is_applicable(input):
             continue
 
         document, errors = converter.convert(input)
-        next_stage.append(document.to_json_file(output))
-
         all_errors += errors
 
-    for path in next_stage:
-        all_errors += process(path, output)
+        all_errors += process(document.to_json_file(output), output)
+        break
 
     return all_errors
