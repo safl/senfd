e26586f9521de25804e365deb9829285b25150e8
Simon A. F. Lund <os@safl.dk>
2024-07-29 10:20:10 +0200

    feat(tables): add docstring ot Table and add HeaderTable repr.
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- src/senfd/tables.py
+++ src/senfd/tables.py
@@ -13,4 +13,18 @@ class Row(BaseModel):
 
 
 class Table(BaseModel):
+    """
+    Tabular data in the most raw form, can be irregular, that is, varying amount of
+    cells in each row.
+    """
     rows: List[Row] = Field(default_factory=list)
+
+
+class HeaderTable(Table):
+    """
+    Header tables are regular, that is, each row has the same amount of cells and cells
+    are named. What the actual content is needs further processing.
+    """
+
+    ncells: int = Field(default_factory=int)
+    headers: List[str] = Field(default_factory=list)
